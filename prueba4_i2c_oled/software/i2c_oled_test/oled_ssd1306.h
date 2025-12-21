fa77de1a04a4b646d72c03dc0e3a205b0606b441
Alvatr0n2071 <alvarodosar@gmail.com>
2025-12-20 23:39:15 -0500

    Update oled_ssd1306.h

--- prueba4_i2c_oled/software/i2c_oled_test/oled_ssd1306.h
+++ prueba4_i2c_oled/software/i2c_oled_test/oled_ssd1306.h
@@ -6,15 +6,15 @@
 
 typedef struct {
   ALT_AVALON_I2C_DEV_t *i2c;
-  uint8_t addr;   // 0x3C o 0x3D
+  uint8_t addr;
 } oled_t;
 
 int  oled_begin(oled_t *o, ALT_AVALON_I2C_DEV_t *i2c, uint8_t addr);
-int  oled_begin_auto(oled_t *o, ALT_AVALON_I2C_DEV_t *i2c); // prueba 0x3C y 0x3D
+int  oled_begin_auto(oled_t *o, ALT_AVALON_I2C_DEV_t *i2c);
 void oled_clear(oled_t *o);
 void oled_set_cursor(oled_t *o, uint8_t page, uint8_t col);
 void oled_putc(oled_t *o, char c);
 void oled_print(oled_t *o, const char *s);
-void oled_print_line(oled_t *o, uint8_t line, const char *s); // line 0..7
+void oled_print_line(oled_t *o, uint8_t line, const char *s);
 
 #endif
